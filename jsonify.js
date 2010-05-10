15e2ceb9344f98ae2fa75ce1c9ef26a66d9e174b
Aaron Forsander <aaron.forsander@gmail.com>
2010-05-10 17:51:49 -0500

    API changed and code reworked

--- jsonify.js
+++ jsonify.js
@@ -3,13 +3,23 @@
 Drupal.jsonify = {
 
   /**
-   * Performs and AJAX GET request on the URL returning
+   * Performs an AJAX GET request on the URL returning
    * the results as JSON.  If a callback is supplied, the 
    * request is asynchronous. If not, the request is synchronous.
    */
   get: function(url, callback) {
     if (typeof callback == "function") {
-      $.getJSON(url, callback);
+      $.ajax({
+        type: "GET",
+        url: url,
+        success: function(data) {
+          callback(data);
+        },
+        error: function() {
+          callback(false);
+        },
+        dataType: "json"
+      });
     }
     else {
       var result;
@@ -22,50 +32,23 @@ Drupal.jsonify = {
           result = data;
         },
         error: function(data) {
-          result = data;
+          result = false;
         },
         dataType: "json"
       });
       
       return result;
     }
-  }, 
-  
-  /**
-   * Retrieve a node object
-   */
-  node: function(opts, callback) {
-    // Allow function to be called with just the nid
-    if (typeof opts != "object") {
-      opts = {nid: opts};
-    }
-    
-    // Override defaults
-    var options = $.extend({ nid: 0 }, opts);
-    
-    var url = Drupal.settings.basePath + "jsonify/node/" + options.nid;
-    
-    return Drupal.jsonify.get(url, callback);
   },
   
-  /**
-   * Retrieve a comment object.
-   */
-  comment: function(opts, callback) {
-    // Allow function to be called with just the nid
-    if (typeof opts != "object") {
-      opts = {cid: opts};
-    }
+  load: function(type, id, callback) {
+    var urls = {
+      node: Drupal.settings.basePath + "jsonify/node/" + id,
+      comment: Drupal.settings.basePath + "jsonify/comment/" + id
+    };
     
-    // Override defaults
-    var options = $.extend({ cid: 0 }, opts);
-    
-    var url = Drupal.settings.basePath + "jsonify/comment/" + options.cid;
-    
-    return Drupal.jsonify.get(url, callback);
+    return !(type in urls) || Drupal.jsonify.get(urls[type], callback);
   }
-  
 };
 
 })(jQuery, Drupal);
-
